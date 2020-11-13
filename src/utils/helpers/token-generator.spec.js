e6865bdb9441742557f57122c96d456eeea18940
Vitor Lourenço <vitor.brother17@outlook.com.br>
2020-11-13 16:13:46 -0300

    ensure TokenGenerator thows if no id is provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -10,6 +10,9 @@ class TokenGenerator {
     if (!this.secret) {
       throw new MissingParamError('secret')
     }
+    if (!id) {
+      throw new MissingParamError('id')
+    }
     return jwt.sign(id, this.secret)
   }
 }
@@ -43,5 +46,11 @@ describe('Email Validator', () => {
     const sut = new TokenGenerator()
     const promisse = sut.generate('any_id')
     expect(promisse).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('Should throw if no id is provided', async () => {
+    const sut = makeSut()
+    const promisse = sut.generate()
+    expect(promisse).rejects.toThrow(new MissingParamError('id'))
   })
 })
