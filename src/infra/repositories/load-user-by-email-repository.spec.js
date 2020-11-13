39b1cb04844c197e1ddc4708531ebaa877237448
Vitor Lourenço <vitor.brother17@outlook.com.br>
2020-11-13 17:44:36 -0300

    remove s in loadUserByEmail

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -5,7 +5,7 @@ class LoadUserByEmailRepository {
 }
 
 describe('LoadUserByEmail Repository', () => {
-  test('Should return null if no user is founds', async () => {
+  test('Should return null if no user is found', async () => {
     const sut = new LoadUserByEmailRepository()
     const user = await sut.load('invalid_email@mail.com')
     expect(user).toBeNull()
