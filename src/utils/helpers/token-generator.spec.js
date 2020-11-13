c7b918c347dca11e79e37ea9ea357d2ba1ec693e
Vitor Lourenço <vitor.brother17@outlook.com.br>
2020-11-13 16:12:22 -0300

    ensure TokenGenerator thows if no secret is provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const MissingParamError = require('../errors/missing-param-error')
 
 class TokenGenerator {
   constructor (secret) {
@@ -6,6 +7,9 @@ class TokenGenerator {
   }
 
   async generate (id) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
     return jwt.sign(id, this.secret)
   }
 }
@@ -33,5 +37,11 @@ describe('Email Validator', () => {
     await sut.generate('any_id')
     expect(jwt.id).toBe('any_id')
     expect(jwt.secret).toBe(sut.secret)
+  })
+
+  test('Should throw if no secret is provided', async () => {
+    const sut = new TokenGenerator()
+    const promisse = sut.generate('any_id')
+    expect(promisse).rejects.toThrow(new MissingParamError('secret'))
   })
 })
